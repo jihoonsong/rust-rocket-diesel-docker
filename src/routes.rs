e8d9033e87c5135f2ddbe6444bc2c5acc7821e8a
Jihoon Song <jihoon.song@voyagerx.com>
2022-02-03 18:05:53 +0900

    Attach a handler to get_all request

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,1 @@
 pub mod todo;
-
-#[get("/")]
-pub fn hello_world() -> &'static str {
-    "hello world"
-}
--- src/routes/todo.rs
+++ src/routes/todo.rs
@@ -1,1 +1,8 @@
+use crate::db;
 
+use rocket::serde::json::Value as JsonValue;
+
+#[get("/")]
+pub async fn get_all(db: db::Db) -> Option<JsonValue> {
+    None
+}
