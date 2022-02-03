def62407a03e4ee3d84f695f44e83e5d6734e972
Jihoon Song <jihoon.song@voyagerx.com>
2022-02-03 19:16:01 +0900

    Use embedded_migrations macro
    This macro is defined in diesel_migrations crate. It enables Rocket
    to run migration on its ignition.

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,29 @@
 pub mod todo;
 
 use rocket::fairing::AdHoc;
+use rocket::{Build, Rocket};
 use rocket_sync_db_pools::{database, diesel};
 
 #[database("postgres")]
 pub struct Db(diesel::PgConnection);
 
+async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    embed_migrations!("migrations");
+
+    let db = Db::get_one(&rocket)
+        .await
+        .expect("Database connection failed");
+    db.run(|db| embedded_migrations::run(db))
+        .await
+        .expect("Diesel migrations failed");
+
+    rocket
+}
+
 pub fn init() -> AdHoc {
     AdHoc::on_ignite("Ignite Diesel and PostgreSQL", |rocket| async {
-        rocket.attach(Db::fairing())
+        rocket
+            .attach(Db::fairing())
+            .attach(AdHoc::on_ignite("Run Diesel migrations", run_migrations))
     })
 }
